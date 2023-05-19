c685abf66c52db541accfd9752abdb2c767e430d
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-19 14:05:02 +0200

    more messing around with arrays

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const myArray = [1, 2, 3];
 myArray[2] = 10;
-myArray[3] = (20);
+myArray[3] = 20;
 console.log(myArray);
 
+////////////////////////////////////////
+
+const instruments = ["piano", "guitar", "drums", "bass"];
+console.log(instruments);
+instruments[3] = "bizzzasse";
+console.log(instruments);
+let bass = instruments[3];
+console.log(bass);
+
+/////////////////////////////////////////
+
+const numberArray = [1, 2, 3, 4];
+let sumOfNumberArray = numberArray[0] +
+                       numberArray[1] +
+                       numberArray[2] +
+                       numberArray[3];
+console.log(sumOfNumberArray); 
+
+//////////////////////////////////////////
+
+let sumOfNumberArrayThroughLoop = 0;
+for (i = 0; i < numberArray.length; i++){
+    sumOfNumberArrayThroughLoop += numberArray[i];
+}
+console.log(sumOfNumberArrayThroughLoop);
+
+///////////////////////////////////////////
+const arrayOfWords = ["words", "word", "ohword?"]
+console.log(arrayOfWords);  // prints the actual array
+console.log(arrayOfWords.toString());   // creates a string with the individual
+                                        //and separates them with commas (but no spaces)
+const someNumbers = [53, 65, 78];
+console.log(someNumbers.toString());
