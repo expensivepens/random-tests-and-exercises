ec547495d99530d8d049adefcedc30becf0ceeea
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-24 19:32:10 +0200

    Add some loop exercises

--- loops.js
+++ loops.js
@@ -51,21 +51,65 @@ console.log(initialVariable3); // 3
 
 //////////////////////////////////////////////////////////////////
 
-exit: for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        let input = prompt(`Value at coords (${i},${j})`, '');
-        if (!input){
-            break exit;
-        }
-    }
-  }
-alert('Done!');
+// exit: for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//         let input = prompt(`Value at coords (${i},${j})`, '');
+//         if (!input){
+//             break exit;
+//         }
+//     }
+//   }
+// alert('Done!');
 
 ///////////////////////////////////////////////////////////////////
 
-while(true){
-    let input = prompt("Name?");
-    if (input) break;
+// while(true){
+//     let input = prompt("Name?");
+//     if (input) break;
+// }
+
+///////////////////////////////////////////////////////////////////
+
+let a = 0;
+while (a++ < 5){  // returns 0, then increments by 1 .... returns 4, which passes the test, then increments
+    console.log(a);  // logs 1, 2, 3, 4, 5
 }
 
-///////////////////////////////////////////////////////////////////
+let b = 0;
+while (++b < 5){  // increments to 1, then returns 1 ...
+    console.log(b); // 1, 2, 3, 4 
+}
+
+////////////////////////////////////////////////////////////////////
+function promptForGreaterThan100(){
+    while(true){
+        let input = prompt("Number greater than 100?");
+        if (!input || input > 100){
+            break;
+        }
+    }
+}
+/////////////////////////////////////////////////////////////////////
+function doWhileGreaterThan100(){
+    do{
+       let input = prompt("Number greater than 100?");
+    } while (input <= 100 && input);
+}
+
+//////////////////////////////////////////////////////////////////
+
+function printPrimeNumbers(n){
+    let listOfPrimeNumbers = "2, ";
+    for (let i = 2; i <= n; i++){
+        for (let j = 2; j < i; j++){
+            if(i % j == 0){
+                break;
+            }
+            if(j == i - 1){
+                listOfPrimeNumbers += `${i}, `;
+            }
+        }
+    }
+    let finalResult = listOfPrimeNumbers.substring(0, listOfPrimeNumbers.length - 2) + ".";
+    console.log(finalResult);
+}
