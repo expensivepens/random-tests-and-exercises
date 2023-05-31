716d6dc4fa30212b9b5b1ea2029519fd0e61f52d
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-31 14:35:28 +0200

    Add a new <div> with a <button> 'ew Button' and give a click event listener to said button

--- events.js
+++ events.js
@@ -10,3 +10,16 @@ buttons[0].onclick = () => alert("Method 2");
 
 buttons[1].addEventListener("click", ()=> alert("Method 3"));
 buttons[1].addEventListener("click", ()=> alert("second message"));
+
+newDiv = document.createElement("div");
+newDiv.style.marginTop = "15px";
+newButton = document.createElement("button");
+newButton.textContent = "New Button";
+newDiv.appendChild(newButton);
+document.body.appendChild(newDiv);
+
+function alertMessage(){
+    alert("This is the message");
+}
+
+newButton.addEventListener("click", alertMessage);
