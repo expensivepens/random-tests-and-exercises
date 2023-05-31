802f2030c0ad714cd877fd45b26059c8f1c59253
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-05-31 13:39:14 +0200

    Remove <button> 'method 1' from the buttons array

--- events.js
+++ events.js
@@ -1,8 +1,8 @@
 const buttons = [];
 
-for (i = 0; i < 3; i++){
+for (i = 0; i < 2; i++){
     buttons[i] = document.createElement("button");
-    buttons[i].textContent = "Method " + i;
+    buttons[i].textContent = "Method " + (i + 2);
     document.body.appendChild(buttons[i]);
 }
 
