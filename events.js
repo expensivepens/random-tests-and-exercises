1a93da0f0d1bf60abdfb3fc4f3e3b0f0761129d3
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-06-01 21:35:49 +0200

    Add a small clicking game

--- events.js
+++ events.js
@@ -23,3 +23,28 @@ function alertMessage(){
 }
 
 newButton.addEventListener("click", alertMessage);
+newButton.addEventListener("click", (e)=> e.target.style.backgroundColor = "blue");
+
+score = 0;
+gameDiv = document.createElement("div");
+gameDiv.style.marginTop = "50px";
+gameButtons = []
+for (i = 0; i < 3; i++){
+    gameButtons[i] = document.createElement("button");
+    gameButtons[i].textContent = `Add ${i} point`;
+    if(i > 1) gameButtons[i].textContent += "s";
+    gameDiv.appendChild(gameButtons[i]);
+}
+scoreParagraph = document.createElement("p");
+scoreParagraph.textContent = score;
+gameDiv.appendChild(scoreParagraph);
+document.body.appendChild(gameDiv);
+
+allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) =>{
+    button.addEventListener("click", (event)=>{
+        score += +event.target.textContent.replace(/[^0-9]/g, "");
+        if (score >= 100) scoreParagraph.textContent = "You win!"
+        else scoreParagraph.textContent = score;
+    })
+})
